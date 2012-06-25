094e1733ae5ecc3bb91ebff3f050118931f20e63
Philip de Nier <philipn@rd.bbc.co.uk>
2012-06-25 17:58:42 +0100

    build: identation cosmetics and fix comment

--- gen_scm_version.sh
+++ gen_scm_version.sh
@@ -2,7 +2,7 @@
 # Based on patch from Peter Hutterer sent to X.Org
 #    http://lists.x.org/archives/xorg-devel/2010-April/007415.html
 #
-# Get the git version if possible and store it in $GITFILE if it differs to
+# Get the git version if possible and store it in $SCM_VER_FILE if it differs to
 # the one contained already.
 
 SCM_VER_FILE="bmx_scm_version.h"
@@ -16,16 +16,16 @@ if command -v git \&>/dev/null; then
   fi
 fi
 if test -z "$GIT_VER"; then
-	exit 0
+  exit 0
 fi
 
 OUTSTR="#define BMX_SCM_VERSION \"$GIT_VER\""
 
 if test -e "$SCM_VER_FILE"; then
-    EXISTING_STR=`cat $SCM_VER_FILE`
-    if test "$EXISTING_STR" = "$OUTSTR"; then
-        exit 0
-    fi
+  EXISTING_STR=`cat $SCM_VER_FILE`
+  if test "$EXISTING_STR" = "$OUTSTR"; then
+    exit 0
+  fi
 fi
 
 echo "$OUTSTR" > $SCM_VER_FILE
