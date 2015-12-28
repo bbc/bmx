326c2c83169587ed8a2a34137faf15ca434f8f0b
Philip de Nier <philipn@users.sourceforge.net>
2015-12-28 16:45:00 +0000

    test:as11: simplify to use a single script file

---
+++ test/as11/test.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+base=$(dirname $0)
+
+md5tool=../file_md5
+
+testdir=..
+appsdir=../../apps
+tmpdir=/tmp/as11test_temp$$
+sampledir=/tmp/
+
+
+create_test_file()
+{
+    $testdir/create_test_essence -t 42 -d 24 $tmpdir/audio
+    $testdir/create_test_essence -t $1 -d 24 $tmpdir/video
+
+    $appsdir/raw2bmx/raw2bmx \
+        --regtest \
+        -t as11op1a \
+        -f 25 \
+        -y 09:58:00:00 \
+        -o $3 \
+        --dm-file as11 $base/as11_core_framework.txt \
+        --dm-file dpp $base/ukdpp_framework.txt \
+        --seg $base/as11_segmentation_framework.txt \
+        --afd 10 -a 16:9 --$2 $tmpdir/video \
+        -q 24 --locked true --pcm $tmpdir/audio \
+        -q 24 --locked true --pcm $tmpdir/audio \
+        -q 24 --locked true --pcm $tmpdir/audio \
+        -q 24 --locked true --pcm $tmpdir/audio \
+        >/dev/null
+}
+
+
+check()
+{
+    create_test_file $1 $2 $tmpdir/test.mxf &&
+        $md5tool < $tmpdir/test.mxf > $tmpdir/test.md5 &&
+        diff $tmpdir/test.md5 $base/$2.md5
+}
+
+create_data()
+{
+    create_test_file $1 $2 $tmpdir/test.mxf &&
+        $md5tool < $tmpdir/test.mxf > $base/$2.md5
+}
+
+create_sample()
+{
+    create_test_file $1 $2 $sampledir/as11_$2.mxf
+}
+
+
+check_all()
+{
+    check 7 avci100_1080i && check 11 d10_50
+}
+
+create_data_all()
+{
+    create_data 7 avci100_1080i && create_data 11 d10_50
+}
+
+create_sample_all()
+{
+    create_sample 7 avci100_1080i && create_sample 11 d10_50
+}
+
+
+mkdir -p $tmpdir
+
+if test "$1" = "create_data" ; then
+    create_data_all
+elif test "$1" = "create_sample" ; then
+    create_sample_all
+else
+    check_all
+fi
+res=$?
+
+rm -Rf $tmpdir
+
+exit $res
