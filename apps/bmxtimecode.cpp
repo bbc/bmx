a1bcfe2f2b6ff7ea2755e8fc54e09272c0919bbd
Philip de Nier <philipn@rd.bbc.co.uk>
2016-09-09 13:50:11 +0100

    bmxtimecode: replace --drop/non-drop with --output param
    The --output parameter value determines whether the output is a
    drop frame timecode, non-drop frame timecode or a frame count. The
    default is to output a count if the input is a timecode or a non-drop
    frame timecode if the input is a count.

--- apps/bmxtimecode.cpp
+++ apps/bmxtimecode.cpp
@@ -65,24 +65,38 @@ static bool parse_frame_rate(const char *rate_str, Rational *frame_rate)
 
 static void usage(const char *name)
 {
-    fprintf(stderr, "%s [--drop] [--non-drop] [--rate (<n>|<n>/<d>)] (all|<timecode>|<frame count>)\n", name);
+    fprintf(stderr, "%s [--output tc-drop|tc-non-drop|count] [--rate (<n>|<n>/<d>)] (all|<timecode>|<frame count>)\n", name);
 }
 
 int main(int argc, const char **argv)
 {
+    bool output_set = false;
+    bool output_timecode = false;
     bool drop_frame = false;
-    bool drop_frame_set = false;
     Rational frame_rate = FRAME_RATE_25;
     const char *value_str = 0;
     int cmdln_index = 1;
 
     for (cmdln_index = 1; cmdln_index < argc; cmdln_index++) {
-        if (strcmp(argv[cmdln_index], "--drop") == 0) {
-            drop_frame = true;
-            drop_frame_set = true;
-        } else if (strcmp(argv[cmdln_index], "--non-drop") == 0) {
-            drop_frame = false;
-            drop_frame_set = true;
+        if (strcmp(argv[cmdln_index], "--output") == 0) {
+            if (cmdln_index + 1 >= argc) {
+                usage(argv[0]);
+                return 1;
+            }
+            if (strcmp(argv[cmdln_index + 1], "tc-drop") == 0) {
+                output_timecode = true;
+                drop_frame      = true;
+            } else if (strcmp(argv[cmdln_index + 1], "tc-non-drop") == 0) {
+                output_timecode = true;
+                drop_frame      = false;
+            } else if (strcmp(argv[cmdln_index + 1], "count") == 0) {
+                output_timecode = false;
+            } else {
+                usage(argv[0]);
+                return 1;
+            }
+            output_set = true;
+            cmdln_index++;
         } else if (strcmp(argv[cmdln_index], "--rate") == 0) {
             if (cmdln_index + 1 >= argc) {
                 usage(argv[0]);
@@ -106,28 +120,35 @@ int main(int argc, const char **argv)
         value_str = argv[cmdln_index];
 
 
-    Timecode timecode;
-    int64_t offset;
-    if (!value_str) {
-        timecode.Init(frame_rate, drop_frame, 0);
+    if (value_str) {
+        Timecode timecode;
+        if (parse_timecode(value_str, frame_rate, &timecode)) {
+          if (!output_set)
+              output_timecode = false;
+          if (output_timecode)
+              timecode.Init(frame_rate, drop_frame, timecode.GetOffset());
+        } else {
+            int64_t offset;
+            if (sscanf(value_str, "%" PRId64, &offset) != 1) {
+                fprintf(stderr, "Failed to parse timecode or frame offset\n");
+                return 1;
+            }
+            timecode.Init(frame_rate, drop_frame, offset);
+            if (!output_set)
+                output_timecode = true;
+        }
+        if (output_timecode)
+            printf("%s\n", get_timecode_string(timecode).c_str());
+        else
+            printf("%" PRId64 "\n", timecode.GetOffset());
+    } else {
+        Timecode timecode(frame_rate, drop_frame, 0);
         int64_t max_offset = timecode.GetMaxOffset();
         int64_t i;
         for (i = 0; i < max_offset; i++) {
             timecode.Init(frame_rate, drop_frame, i);
             printf("%8" PRId64 ": %s\n", i, get_timecode_string(timecode).c_str());
         }
-    } else if (parse_timecode(value_str, frame_rate, &timecode)) {
-        if (drop_frame_set) {
-            timecode.Init(frame_rate, drop_frame, timecode.GetOffset());
-            printf("%s\n", get_timecode_string(timecode).c_str());
-        }
-        printf("%" PRId64 "\n", timecode.GetOffset());
-    } else if (sscanf(value_str, "%" PRId64, &offset) == 1) {
-        timecode.Init(frame_rate, drop_frame, offset);
-        printf("%s\n", get_timecode_string(timecode).c_str());
-    } else {
-        fprintf(stderr, "Failed to parse timecode or frame offset\n");
-        return 1;
     }
 
     return 0;
