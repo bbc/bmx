589a906772290793d226bf32e8acb1e8e0a32538
Philip de Nier <philipn@rd.bbc.co.uk>
2023-01-16 15:52:00 +0000

    Use git describe relative to release tag

--- cmake/git_version.cmake
+++ cmake/git_version.cmake
@@ -1,7 +1,11 @@
-if(TARGET cmake_git_version_tracking)
+if(TARGET bmx_cmake_git_version_tracking)
     return()
 endif()
 
+
+# Set the git release tag pattern to describe relative to
+set(GIT_DESCRIBE_TAG_PATTERN "v${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}")
+set(GIT_WORKING_DIR ${PROJECT_SOURCE_DIR})
 
 # Set to ignore git_version errors if there is no .git directory
 set(GIT_FAIL_IF_NONZERO_EXIT FALSE)
@@ -11,19 +15,20 @@ include(FetchContent)
 set(git_version_deps_source "${PROJECT_SOURCE_DIR}/deps/cmake-git-version-tracking")
 
 if(EXISTS ${git_version_deps_source})
-    FetchContent_Declare(cmake_git_version_tracking
+    FetchContent_Declare(bmx_cmake_git_version_tracking
         SOURCE_DIR ${git_version_deps_source}
     )
     message("-- Build using git version source: ${git_version_deps_source}")
 else()
-    FetchContent_Declare(cmake_git_version_tracking
+    FetchContent_Declare(bmx_cmake_git_version_tracking
         GIT_REPOSITORY "https://github.com/andrew-hardin/cmake-git-version-tracking.git"
         GIT_TAG "904dbda1336ba4b9a1415a68d5f203f576b696bb"
+        PATCH_COMMAND git apply --ignore-whitespace "${CMAKE_CURRENT_LIST_DIR}/git_version_904dbda.patch"
     )
 endif()
 
-FetchContent_GetProperties(cmake_git_version_tracking)
-if(NOT cmake_git_version_tracking_POPULATED)
-    FetchContent_Populate(cmake_git_version_tracking)
-    add_subdirectory(${cmake_git_version_tracking_SOURCE_DIR} ${cmake_git_version_tracking_BINARY_DIR})
+FetchContent_GetProperties(bmx_cmake_git_version_tracking)
+if(NOT bmx_cmake_git_version_tracking_POPULATED)
+    FetchContent_Populate(bmx_cmake_git_version_tracking)
+    add_subdirectory(${bmx_cmake_git_version_tracking_SOURCE_DIR} ${bmx_cmake_git_version_tracking_BINARY_DIR})
 endif()
